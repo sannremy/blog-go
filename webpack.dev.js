8ded30c783858a448df2ad0a414097dbb6816edf
Sann-Remy Chea <sannremy.chea@gmail.com>
2019-02-25 07:55:21 +0100

    Moved nodemon and livereload in dev config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,19 @@
+const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+const NodemonPlugin = require('nodemon-webpack-plugin');
+const LiveReloadPlugin = require('webpack-livereload-plugin');
+
 module.exports = merge(common, {
-  mode: 'development'
+  mode: 'development',
+  plugins: [
+    new NodemonPlugin({
+      watch: path.resolve('./src'),
+      ignore: ['./src/static', './src/views'],
+      verbose: true,
+      script: './src/app.js'
+    }),
+    new LiveReloadPlugin()
+  ]
 });
