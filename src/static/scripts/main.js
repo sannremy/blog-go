278c08a9f5f8ac43197c42349f84e6873bd00ca2
Sann-Remy Chea <sannremy.chea@gmail.com>
2019-03-01 07:59:13 +0100

    Improved loader

--- src/static/scripts/main.js
+++ src/static/scripts/main.js
@@ -2,7 +2,7 @@ import '../styles/main.scss';
 
 import Routes from './routes';
 import page from 'page';
-import Pace from 'pace-progress';
+import 'pace-progress';
 import FontFaceObserver from 'fontfaceobserver';
 
 const fontA = new FontFaceObserver('Work Sans');
