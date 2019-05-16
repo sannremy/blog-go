71b27e5e1c7f65d851062178887a98df77ba6cab
Sann-Remy Chea <sannremy.chea@gmail.com>
2019-05-16 21:24:49 +0200

    Added middleware to redirect without www

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
   "io"
   "net/http"
+  "net/url"
+  "strings"
   "github.com/gorilla/mux"
   "google.golang.org/appengine" // Required external App Engine library
 )
@@ -12,8 +14,30 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
   io.WriteString(w, "src.onl auto deployed!\n")
 }
 
+func RedirectWwwMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    if strings.HasPrefix(r.Host, "www.") {
+      host := strings.TrimLeft(r.Host, "www.")
+
+      redirectedUrl := url.URL{
+        Scheme: "https",
+        Host: host,
+        Path: r.URL.Path,
+        RawQuery: r.URL.RawQuery,
+      } 
+
+      http.Redirect(w, r, redirectedUrl.String(), http.StatusMovedPermanently)
+    }
+
+    next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
   r := mux.NewRouter()
+
+  // Redirect if www
+  r.Use(RedirectWwwMiddleware)
 
   // Index route
   r.HandleFunc("/", IndexHandler)
