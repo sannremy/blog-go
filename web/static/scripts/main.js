9e0d64d3fb437b872995665323a38c59709c49ff
Sann-Remy Chea <sannremy.chea@gmail.com>
2019-07-12 21:12:25 +0200

    Migrated 1st post

--- web/static/scripts/main.js
+++ web/static/scripts/main.js
@@ -4,6 +4,10 @@ import '../styles/main.scss';
 import Prism from 'prismjs/components/prism-core';
 import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-http';
+import 'prismjs/components/prism-markup-templating';
+import 'prismjs/components/prism-php';
+import 'prismjs/components/prism-javascript';
 import 'prismjs/themes/prism.css';
 
 const postElement = document.querySelector('.post');
